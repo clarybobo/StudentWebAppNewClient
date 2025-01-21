17be051e8458d42cb3e6bc1144dd7e46c7844280
sanna <sanna.nyberg@hotmail.se>
2025-01-21 14:08:59 +0100

    Uppdaterat Cors med url till app service

--- StudentWebAppNewClient/Program.cs
+++ StudentWebAppNewClient/Program.cs
@@ -4,26 +4,23 @@ using StudentWebAppNewClient.Data.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Lägg till CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowLocalhost", policy =>
     {
-        policy.WithOrigins("http://localhost:7170", "http://localhost:7246") // Lägg till de URL:er som ska tillåtas
-              .AllowAnyMethod() // Tillåt alla metoder (GET, POST, etc.)
-              .AllowAnyHeader() // Tillåt alla headers
-              .WithHeaders(HeaderNames.ContentType, HeaderNames.Authorization, "x-custom-header"); // Specifika headers
+        policy.WithOrigins("http://localhost:7170", "http://localhost:7246", "https://studentwebappnewclient.azurewebsites.net") 
+              .AllowAnyHeader() 
+              .WithHeaders(HeaderNames.ContentType, HeaderNames.Authorization, "x-custom-header"); 
     });
 });
 
 // Add services to the container.
 builder.Services.AddScoped<IStudentService, StudentService>();
+
 // Hårdkodad BaseUrl
 builder.Services.AddHttpClient<IStudentService, StudentService>(client =>
-{
-    // Ange direkt BaseUrl här för API:t 
-    // UPPDATERAD med aktuell API-länk från Azure
-    client.BaseAddress = new Uri("https://studentapi-app-new.calmtree-16028aa1.northeurope.azurecontainerapps.io"); // Exempel för lokal utveckling
+{  
+    client.BaseAddress = new Uri("https://studentapi-app-new.calmtree-16028aa1.northeurope.azurecontainerapps.io"); 
     ;
 });
 
@@ -40,22 +37,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-//Suzies CORS 
-//app.UseCors(policy =>
-//policy.WithOrigins("https://localhost:7246/", "https://localhost:7277/", "https://studentwebappclient20250120153311.azurewebsites.net")
-//.AllowAnyMethod()
-//.WithHeaders(HeaderNames.ContentType, HeaderNames.Authorization, "x-custom-header"));
-
-//TODO: Lägg till rätt länk från Azure! 
-// AKTUELL CORS! 
-//WebApp https://localhost:7170 
-//Api https://localhost:7246
-//Azure 
-//app.UseCors(policy =>
-//policy.WithOrigins("https://localhost:7246", "https://localhost:7170", "http://localhost:5289")
-//.AllowAnyMethod()
-//.WithHeaders(HeaderNames.ContentType, HeaderNames.Authorization, "x-custom-header"));
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
